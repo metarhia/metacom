80749117625b31395bb4ab091f1fb9aa292c25e6
tshemsedinov <timur.shemsedinov@gmail.com>
2018-04-19 05:30:54 +0300

    Add transports: ws, wss, tcp, tls
    Refs: https://github.com/metarhia/metacom/issues/4
    PR-URL: https://github.com/metarhia/metacom/pull/6

--- metacom.js
+++ metacom.js
@@ -1,9 +1,6 @@
 'use strict';
 
-const submodules = [
-  'client', // metacom client connection
-  'server' // metacom server
-].map(path => require('./lib/' + path));
-
-const { client } = submodules[0];
-module.exports = Object.assign(client, ...submodules);
+module.exports = {
+  connect: require('./lib/client'),
+  listen: require('./lib/server')
+};
