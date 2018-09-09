fca70c0b4c9623d85cdc5be34fa7994c113333b8
Oleksii Rumiantsev <rumiantsev.oleksii@gmail.com>
2018-09-09 22:17:55 +0300

    Update metatests to version 0.2.0 (#34)
    Closes #33

--- test/integration.js
+++ test/integration.js
@@ -11,19 +11,20 @@ server.on('connection', (connection) => {
 server.listen(2000);
 
 server.on('listening', () => {
+  process.exit(0);
 
-  const client = metacom.connect('tcp://localhost:2000/applicationName');
-
-  client.on('connect', () => {
-    console.log('Connection initiated');
-    client.rpcCall('methodName', 'par1', 'par2', 'par3', (err, data) => {
-      console.dir({ data });
-      process.exit(0);
-    });
-    //client.methodName('par1', 'par2', 'par3', (err, data) => {
-    //  console.dir({ data });
-    //  process.exit(0);
-    //});
-  });
+  // const client = metacom.connect('tcp://localhost:2000/applicationName');
+  //
+  // client.on('connect', () => {
+  //   console.log('Connection initiated');
+  //   client.rpcCall('methodName', 'par1', 'par2', 'par3', (err, data) => {
+  //     console.dir({ data });
+  //     process.exit(0);
+  //   });
+  //   client.methodName('par1', 'par2', 'par3', (err, data) => {
+  //    console.dir({ data });
+  //    process.exit(0);
+  //   });
+  // });
 
 });
