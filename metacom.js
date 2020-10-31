8a1a478db9a89f8ae6008ea00a77a49cda3d2e96
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-11-01 00:56:00 +0200

    Export Semaphore
    PR-URL: https://github.com/metarhia/metacom/pull/80

--- metacom.js
+++ metacom.js
@@ -2,6 +2,8 @@
 
 const { Server } = require('./lib/server.js');
 const { Metacom } = require('./lib/client.js');
+const { Semaphore } = require('./lib/semaphore.js');
 
 module.exports = Metacom;
 module.exports.Server = Server;
+module.exports.Semaphore = Semaphore;
