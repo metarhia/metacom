378e7674e027f2d088eca9e5201760d61095ac97
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-11-01 01:08:57 +0200

    Backport Channel from impress
    Refs: https://github.com/metarhia/impress/issues/1236
    
    PR-URL: https://github.com/metarhia/metacom/pull/81

--- metacom.js
+++ metacom.js
@@ -1,9 +1,11 @@
 'use strict';
 
-const { Server } = require('./lib/server.js');
 const { Metacom } = require('./lib/client.js');
+const { Server } = require('./lib/server.js');
+const { Channel } = require('./lib/channel.js');
 const { Semaphore } = require('./lib/semaphore.js');
 
 module.exports = Metacom;
 module.exports.Server = Server;
+module.exports.Channel = Channel;
 module.exports.Semaphore = Semaphore;
