0fa0772e83d58f491bb0f44ac97bbed9e2d558c5
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-12-14 02:18:11 +0200

    Implement http transport strategy for browser client
    Closes: https://github.com/metarhia/metacom/issues/98
    
    PR-URL: https://github.com/metarhia/metacom/pull/107

--- dist/metacom.js
+++ dist/metacom.js
@@ -183,7 +183,35 @@ class WebsocketTransport extends Metacom {
   }
 }
 
-class HttpTransport extends Metacom {}
+class HttpTransport extends Metacom {
+  async open() {
+    this.active = true;
+    this.connected = true;
+  }
+
+  close() {
+    this.active = false;
+    this.connected = false;
+  }
+
+  send(data) {
+    this.lastActivity = new Date().getTime();
+    fetch(this.url, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: data,
+    }).then(res => {
+      const { status } = res;
+      if (status === 200) {
+        return res.text().then(packet => {
+          if (packet.error) throw new MetacomError(packet.error);
+          this.message(packet);
+        });
+      }
+      throw new Error(`Status Code: ${status}`);
+    });
+  }
+}
 
 Metacom.transport = {
   ws: WebsocketTransport,
