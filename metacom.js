8e69f051a51413bdac665842007540b306ab51e6
Dmytro Nechai <nechaido@gmail.com>
2021-06-18 15:48:03 +0300

    Update Metacom exports

--- metacom.js
+++ metacom.js
@@ -4,6 +4,6 @@ const { Metacom } = require('./lib/client.js');
 const { Server } = require('./lib/server.js');
 const { Channel } = require('./lib/channel.js');
 
-module.exports = Metacom;
+module.exports.Metacom = Metacom;
 module.exports.Server = Server;
 module.exports.Channel = Channel;
