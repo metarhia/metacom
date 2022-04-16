e7978e5e5d2369d558b850ffb594ddf65699915b
georgolden <georgoldenb@gmail.com>
2022-04-17 00:06:25 +0200

    Remove duplicated EventEmitter in MetacomInterface

--- dist/metacom.js
+++ dist/metacom.js
@@ -19,21 +19,9 @@ class MetacomError extends Error {
   }
 }
 
-class MetacomInterface {
+class MetacomInterface extends EventEmitter {
   constructor() {
-    this._events = new Map();
-  }
-
-  on(name, fn) {
-    const event = this._events.get(name);
-    if (event) event.add(fn);
-    else this._events.set(name, new Set([fn]));
-  }
-
-  emit(name, ...args) {
-    const event = this._events.get(name);
-    if (!event) return;
-    for (const fn of event.values()) fn(...args);
+    super();
   }
 }
 
