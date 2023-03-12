1d68feb92c97920cac0b086bedebaa701a90f69f
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2023-03-12 22:42:51 +0200

    fixup! Add tests for `EventEmitter` polyfill

--- test/events.js
+++ test/events.js
@@ -2,9 +2,18 @@
 
 const metatests = require('metatests');
 const EventEmitter = require('../lib/events.js');
+const { MetacomInterface } = require('../lib/client.js');
 
 metatests.test('EventEmitter polyfill', async (test) => {
   const ee = new EventEmitter();
+  ee.on('name', () => {
+    test.end();
+  });
+  ee.emit('name');
+});
+
+metatests.test('MetacomInterface', async (test) => {
+  const ee = new MetacomInterface();
   ee.on('name', () => {
     test.end();
   });
