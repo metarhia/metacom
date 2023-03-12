7e832e7ab7cdafb3f5120e68d27572d99df26948
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2023-03-12 22:55:46 +0200

    fixup! Add tests for `EventEmitter` polyfill

--- test/events.js
+++ test/events.js
@@ -6,16 +6,29 @@ const { MetacomInterface } = require('../lib/client.js');
 
 metatests.test('EventEmitter polyfill', async (test) => {
   const ee = new EventEmitter();
-  ee.on('name', () => {
+  ee.on('eventName', () => {
     test.end();
   });
-  ee.emit('name');
+  ee.emit('eventName');
 });
 
 metatests.test('MetacomInterface', async (test) => {
   const ee = new MetacomInterface();
-  ee.on('name', () => {
+  ee.on('eventName', () => {
     test.end();
   });
-  ee.emit('name');
+  ee.emit('eventName');
+});
+
+metatests.test('MetacomInterface subscribe *', async (test) => {
+  test.plan(3);
+  const ee = new MetacomInterface();
+  ee.on('*', (name, data) => {
+    test.strictEqual(name, 'eventName');
+    test.strictEqual(data, { data: 100 });
+  });
+  ee.on('eventName', (data) => {
+    test.strictEqual(data, { data: 100 });
+  });
+  ee.emit('eventName', { data: 100 });
 });
