a9d813ac6afd714f20b1cafcbed96486b449fe17
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2023-04-06 21:03:35 +0300

    Drop `Channel`, move functionality to `Client` and `Transport`
    PR-URL: https://github.com/metarhia/metacom/pull/384

--- metacom.js
+++ metacom.js
@@ -2,8 +2,5 @@
 
 const { Metacom } = require('./lib/client.js');
 const { Server } = require('./lib/server.js');
-const { Channel } = require('./lib/channel.js');
 
-module.exports.Metacom = Metacom;
-module.exports.Server = Server;
-module.exports.Channel = Channel;
+module.exports = { Metacom, Server };
