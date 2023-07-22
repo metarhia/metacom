7a5fc8d119253638c7d5130e7daff1a81e716b5f
mprudnik <maksymprudnik@gmail.com>
2023-07-22 18:15:15 +0300

    Add stream tests for client

--- test/client.js
+++ test/client.js
@@ -1,10 +1,12 @@
 'use strict';
 
 const timers = require('node:timers/promises');
+const { Blob } = require('node:buffer');
 const metatests = require('metatests');
 const { WebSocketServer } = require('ws');
 const metautil = require('metautil');
 const { Metacom } = require('../lib/client.js');
+const { chunkEncode, chunkDecode } = require('../lib/streams.js');
 
 const { emitWarning } = process;
 process.emitWarning = (warning, type, ...args) => {
@@ -133,7 +135,7 @@ metatests.test('Client / events', async (test) => {
   test.defer(() => void mockServer.close());
 
   test.beforeEach(async () => {
-    client = Metacom.create('ws://localhost:8001/', { callTimeout: 150 });
+    client = Metacom.create('ws://localhost:8001/');
     await client.opening;
     await client.load('test');
   });
@@ -154,5 +156,124 @@ metatests.test('Client / events', async (test) => {
       client.api.test.on('ping', resolve),
     );
     t.strictEqual(ping, { ping: true });
+  });
+});
+
+metatests.test('Client / stream', async (test) => {
+  const storage = new Map();
+
+  const handleBinary = (chunk) => {
+    const { id, payload } = chunkDecode(chunk);
+    const stream = storage.get(id);
+    if (!stream) return;
+    stream.data.push(payload.toString('utf8'));
+  };
+
+  const handleOutgoingStream = async (ws, { id, name, blob }) => {
+    const initPacket = { type: 'stream', id, name, size: blob.size };
+    const endPacket = { type: 'stream', id, status: 'end' };
+    ws.send(JSON.stringify(initPacket));
+    const reader = blob.stream().getReader();
+    let chunk;
+    while (!(chunk = await reader.read()).done) {
+      ws.send(chunkEncode(id, chunk.value));
+    }
+    ws.send(JSON.stringify(endPacket));
+  };
+
+  const handleIncomingStream = ({ id, name, size, status }) => {
+    const stream = storage.get(id);
+    if (status) {
+      if (!stream) throw new Error(`Stream ${id} is not initialized`);
+      if (status === 'end') stream.status = 'ended';
+      if (status === 'terminate') stream.status = 'terminated';
+      return;
+    }
+    const valid = typeof name === 'string' && Number.isSafeInteger(size);
+    if (!valid) throw new Error('Stream packet structure error');
+    if (stream) throw new Error(`Stream ${id} is already initialized`);
+    {
+      const stream = { name, size, data: [], status: 'init' };
+      storage.set(id, stream);
+    }
+  };
+
+  const api = {
+    system: {
+      introspect: { handler: async () => api },
+    },
+    test: {
+      getStreamData: {
+        handler: async ({ id }) => storage.get(id),
+      },
+      download: {
+        handler: async ({ name }, ws) => {
+          const id = 2;
+          const data = 'Some random data for upload to the client';
+          const blob = new Blob([data]);
+          handleOutgoingStream(ws, { id, name, blob });
+          return { id };
+        },
+      },
+    },
+  };
+
+  const mockServer = new WebSocketServer({ port: 8002 });
+  mockServer.on('connection', (ws) => {
+    const pingInterval = setInterval(() => {
+      const packet = { type: 'event', name: 'test/ping', data: { ping: true } };
+      ws.send(JSON.stringify(packet));
+    }, 100);
+    ws.on('close', () => void clearInterval(pingInterval));
+    ws.on('message', async (raw, isBinary) => {
+      if (isBinary) return void handleBinary(raw);
+      const packet = metautil.jsonParse(raw) || {};
+      if (packet.type === 'call' && packet.method === 'system/introspect') {
+        const introspection = { type: 'callback', id: packet.id, result: api };
+        return void ws.send(JSON.stringify(introspection));
+      }
+      if (packet.type === 'stream') return void handleIncomingStream(packet);
+      const { type, id, method, args } = packet;
+      const [unit, name] = method.split('/');
+      if (type !== 'call') return;
+      const result = await api[unit][name].handler(args, ws);
+      ws.send(JSON.stringify({ type: 'callback', id, result }));
+    });
+  });
+
+  /** @type Metacom */
+  let client;
+
+  test.defer(() => void mockServer.close());
+
+  test.beforeEach(async () => {
+    client = Metacom.create('ws://localhost:8002/');
+    await client.opening;
+    await client.load('test');
+  });
+
+  test.afterEach(async () => void client.close());
+
+  test.testAsync('handles file uploades', async (t) => {
+    const data = 'Some random data for upload to the server';
+    const name = 'upload-stream';
+    const blob = new Blob([data]);
+    blob.name = name;
+    const stream = client.createBlobUploader(blob);
+    await stream.upload();
+    const uploadedFile = await client.api.test.getStreamData({ id: stream.id });
+    t.strictEqual(uploadedFile.name, name);
+    t.strictEqual(uploadedFile.size, blob.size);
+    t.strictEqual(uploadedFile.data.join(''), data);
+    t.strictEqual(uploadedFile.status, 'ended');
+  });
+
+  test.testAsync('handles file downloads', async (t) => {
+    const name = 'download-stream';
+    const { id } = await client.api.test.download({ name });
+    const readable = client.getStream(id);
+    const blob = await readable.toBlob();
+    const data = await blob.text();
+    t.strictEqual(data, 'Some random data for upload to the client');
   });
 });
