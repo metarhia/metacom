384999eca664e6218c73da44792483662dffb1fd
Heorhii <margievge@gmail.com>
2025-06-23 21:28:30 +0200

    Clean from debugging

--- test/server.js
+++ test/server.js
@@ -74,12 +74,7 @@ metatests.test('Server / calls', async (test) => {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(packet),
-    })
-      .then((res) => res.json())
-      .catch((err) => {
-        console.log({ EEE: err });
-      });
-    console.log({ response, subtest });
+    }).then((res) => res.json());
 
     subtest.strictEqual(response.id, id);
     subtest.strictEqual(response.type, 'callback');
