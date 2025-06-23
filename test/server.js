98c3e8cb045135d09ab4929e750156acfc1a91cf
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2025-06-23 13:18:40 -0400

    Update test/server.js

--- test/server.js
+++ test/server.js
@@ -55,7 +55,6 @@ metatests.test('Server / calls', async (test) => {
     getHook: noop,
   };
 
-  /** @type Server */
   let server;
 
   test.beforeEach(async () => {
@@ -80,7 +79,7 @@ metatests.test('Server / calls', async (test) => {
     subtest.strictEqual(response.result, `Hello, ${args.name}`);
   });
 
-  test.testAsync('handles WS RPC', async (subtest) => {
+  test.testAsync('WS RPC handles', async (subtest) => {
     const id = 1;
     const args = { name: 'Max' };
     const packet = { type: 'call', id, method: 'test/hello', args };
