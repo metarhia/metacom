179056848637e4bdb5d487a74e1a6e3ecde8d8b3
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2026-04-02 00:08:16 +0300

    Export buildHeaders to use in impress

--- metacom.js
+++ metacom.js
@@ -2,5 +2,6 @@
 
 const { Metacom } = require('./lib/metacom.js');
 const { Server } = require('./lib/server.js');
+const { buildHeaders } = require('./lib/transport.js');
 
-module.exports = { Metacom, Server };
+module.exports = { Metacom, Server, buildHeaders };
